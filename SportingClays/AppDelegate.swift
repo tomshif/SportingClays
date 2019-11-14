52d4b4ce1ed29e4a6cb4f0431065fe723308c12f
Tom Shiflet <tom.shiflet@mylcs.com>
2019-11-14 10:00:16 -0600

    Initial Commit

---
+++ SportingClays/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SportingClays
+//
+//  Created by Tom Shiflet on 11/14/19.
+//  Copyright © 2019 LCS Game Design. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
